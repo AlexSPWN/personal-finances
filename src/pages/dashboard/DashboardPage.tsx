67770b2f9078b351ea149dcb67de4e69b9507728
AlexSPWN <alexeysandulskiy@gmail.com>
2026-01-05 12:13:56 +0200

    Test merging

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -4,7 +4,7 @@ import { logout } from "../../services/authService";
 export const DashboardPage = () => {
   return (
     <>
-      <h1>Dashboard</h1>
+      <h1>Dashboard :)</h1>
       <Link to="admin">Admin</Link>
       <Link to="manager">Manager</Link>
       <button onClick={logout}>Logout</button>
