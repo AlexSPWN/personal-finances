941f7b036958f5fa6b32cfd65c4dc8c11f6950e1
Alex <32991857+AlexSPWN@users.noreply.github.com>
2026-01-05 14:32:29 +0200

    remove New smile (#6)
    * test: trigger PR checks
    
    * Test merging
    
    * Add more changes to Dashboard
    
    * Upd AdminPage
    
    * Add text smile
    
    * remove New Smile

--- src/pages/dashboard/admin/AdminPage.tsx
+++ src/pages/dashboard/admin/AdminPage.tsx
@@ -21,8 +21,7 @@ export const AdminPage = () => {
 
   return (
    <div className="p-4 space-y-4">
-      <h2 className="text-xl font-bold">Admin Panel New smile</h2>
-
+      <h2 className="text-xl font-bold">Admin Panel</h2>
       {/* Tabs */}
       <div className="flex border-b">
         {tabs.map((tab) => (
